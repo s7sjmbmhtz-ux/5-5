d0087ec6b4809d6b43b82d194c24cce5cbde5946
s7sjmbmhtz-ux <s7sjmbmhtz@privaterelay.appleid.com>
2026-08-13 17:22:58 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "5x5-shell-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
